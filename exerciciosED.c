a0407f68ad56604c14c7ab9c94762d4153a29d43
VitorHugoSantos <56173037+VitorHugoSantos@users.noreply.github.com>
2019-10-21 11:00:30 -0300

    Add files via upload

---
+++ exerciciosED.c
@@ -0,0 +1,66 @@
+/*
+Seja L uma lista simplesmente encadeada composta de números inteiros
+cujos nós são l1, l2, l3, ..., ln. Escreva uma programa que,
+percorrendo L, uma única vez, construa três outras lista formadas
+dos seguintes elementos:
+a. l2, l3, ..., ln, l1;
+b. ln, ln-1 ,...,l1;
+c. l1+ln, l2+ln-1, ..., ln/2+ln/2+1; onde n é par e definido pelo usuário
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Lista{
+    int id;
+    struct Lista *proximo;
+};
+
+typedef struct Lista l;
+
+l* crianos(){
+    l *novo = NULL;
+    novo = (l*) malloc(sizeof(l));
+    return novo;
+}
+
+l* inserir_inicio(l* nodo0, int dado){
+    l *n=crianos();
+    n->id=dado;
+    if (nodo0 == NULL){
+        nodo0 = n;
+        n->proximo = NULL;
+    }
+    else{
+        n->proximo = nodo0;
+        nodo0 = n;
+    }
+    return nodo0;
+}
+
+void imprimir(l* nodo0){
+    l *aux = nodo0;
+    while(aux != NULL){
+        printf("\n%d ",aux->id);
+        aux = aux->proximo;
+    }
+}
+
+
+int main(){
+    l *nodo0 = NULL;
+    nodo0 = (l*) malloc(sizeof(l*));
+    nodo0->id = 0;
+
+    int nnos;
+    printf("Digite a quntidade de nós");
+    scanf("%d", &nnos);
+    for(int i = 0; i < nnos; i++){
+        int c=1;
+        crianos(nodo0);
+        inserir_inicio(nodo0,c);
+        c++;
+    } 
+    inserir_inicio(nodo0,10);
+    imprimir(nodo0);
+}
